385e50285eb98e7b02616c84ece478fbbc623992
sebastianrodriguez <sebrod35@purdue.edu>
2025-04-18 23:42:47 -0400

    adding a status line for the vibes

--- lua/theprimeagen/lazy/statusline.lua
+++ lua/theprimeagen/lazy/statusline.lua
@@ -1,13 +1,13 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    options = {
+      theme = "auto",
+      section_separators = "",
+      component_separators = "",
     },
-    opts = {
-        options = {
-            theme = "catppuccin",
-            section_separators = "",
-            component_separators = "",
-        },
-    },
+  },
 }
